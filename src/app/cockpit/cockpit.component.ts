37044bdc56e4b321c2e6adc034855b9b07c97554
nydi <stefan.nydegger@gmail.com>
2018-05-30 19:28:50 +0200

    lecture 69

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -10,7 +10,7 @@ export class CockpitComponent implements OnInit {
   serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output()
   blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  newServerName = '';
+  // newServerName = '';
   newServerContent = '';
 
   constructor() {
@@ -19,18 +19,18 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit(
       {
-        serverName: this.newServerName,
+        serverName: nameInput.value,
         serverContent: this.newServerContent
       });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit(
       {
-        serverName: this.newServerName,
+        serverName: nameInput.value,
         serverContent: this.newServerContent
       });
   }
